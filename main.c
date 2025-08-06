ebdbcbe9e7349b0c5012cb1e9e6de6ad7b46b24d
bendennis06 <130309802+bendennis06@users.noreply.github.com>
2025-08-06 13:34:14 -0700

    Add files via upload
    timer

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 /*
  * goal 					register			Address				bit change
  *
@@ -9,8 +10,13 @@
  */
 
 #include <stdint.h>
-#define RCC_APB2ENR *((uint32_t *)0x40021018)
-#define RCC_APB2ENR_IOPCEN 1<<4
+#define RCC_APB2ENR_REGISTER *((uint32_t *)0x40021018)
+#define RCC_APB2ENR_IOPCEN 1<<4 //ENABLE CLOCK TO GPIOC
+#define RCC_APB2ENR_USART1 1<<14 //ENABLE CLOCK TO USART1
+#define RCC_APB2ENR_IOPAEN 1<<2 //ENABLE CLOCK TO GPIOA
+
+#define GPIOA_CRH_REGISTER *((uint32_t *)0x40010804)
+#define GPIOC_CRH_REGISTER *((uint32_t *)0x40011004)
 
 void delay(uint32_t time){
 	for(int i =0;i<time;i++){
@@ -20,11 +26,22 @@ void delay(uint32_t time){
 
 int main(void)
 {
+	//CLOCK
+	RCC_APB2ENR_REGISTER |= RCC_APB2ENR_USART1; //ENABLE USART1
+	RCC_APB2ENR_REGISTER |= RCC_APB2ENR_IOPCEN; //ENABLE GPIOC(LED)
+	RCC_APB2ENR_REGISTER |= RCC_APB2ENR_IOPAEN; //ENABLE GPIOA(USART PINS)
 
-	RCC_APB2ENR |= RCC_APB2ENR_IOPCEN;
-	//crh
-	*((uint32_t *)0x40011004) &= ~(1<<20);
-	*((uint32_t *)0x40011004) |= (1<<20);
+	//CRH
+	GPIOC_CRH_REGISTER &= ~(1<<20); //LED
+	GPIOC_CRH_REGISTER |= (1<<20);
+	GPIOA_CRH_REGISTER &= ~(0b1111<<4);//PA_9
+	GPIOA_CRH_REGISTER |= (0b1010<<4);
+	GPIOA_CRH_REGISTER &= ~(0b1111<<8);
+	GPIOA_CRH_REGISTER |= (0b0100<<8);
+
+
+
+
 
     /* Loop forever */
 	for(;;){
@@ -35,3 +52,4 @@ int main(void)
 		*((uint32_t *)0x4001100C) &= ~(1<<13);
 	}
 }
+
